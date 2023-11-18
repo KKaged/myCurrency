fc1004ea53f98e26e6c2dbfd189699d3b99400f2
KKaged <dhigaredaleon707@gmail.com>
2023-11-18 13:26:08 +0900

    API works, using axios

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 let crncyOne = document.querySelector("#crncy1");
 
-const USDtoJPYreq = async (crncy1, crncy2) => {
-  try {
-    const req = await fetch(
-      `https://v6.exchangerate-api.com/v6/421cf9898b649f8594231bfd/pair/${crncy1}/${crncy2}`
-    );
-    const thisData = await req.json();
-    crncyOne.innerHTML = thisData.conversion_rate;
-  } catch (e) {
-    console.log("There seems to be an error.", e);
-  }
-};
-USDtoJPYreq("USD", "JPY");
+axios
+  .get(
+    "https://v6.exchangerate-api.com/v6/421cf9898b649f8594231bfd/pair/USD/JPY"
+  )
+  .then((res) => {
+    crncyOne.innerHTML = res.data.conversion_rate;
+  })
+  .catch((e) => {
+    console.log("Error: ", e);
+  });
